417bf47e25877e48eb10aec402e3a3cb4a429f1a
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2015-05-10 21:06:20 -0400

    Changed spirograph-2 and added third
    Added third version. Support for only 2 arms, but put into a function
    that detects when the drawing is complete.

--- Spirograph-2.py
+++ Spirograph-2.py
@@ -9,6 +9,14 @@ canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
 canvas.configure(scrollregion=(-250, -250, 250, 250))
 
+def create_point(coords, canv=canvas):
+    assert len(coords) == 2
+    canv.create_line(coords[0], coords[1], coords[0]+1, coords[1]+1)
+
+def create_line(coords1, coords2, canv=canvas):
+    line = canvas.create_line(coords1[0], coords1[1], coords2[1], coords2[1])
+    return line
+
 def rotate(point, angle, center=(0, 0)):
     counterangle = 360 - angle
     while counterangle > 0: counterangle -= 360
@@ -21,6 +29,87 @@ def rotate(point, angle, center=(0, 0)):
     #Translate point back
     newcoords = (round(rotated[0]+center[0], 1),round(rotated[1]+center[1], 1))
     return newcoords
+
+
+def drawSpiral(arms, lenlist, velocitylist):
+    if not arms == len(lenlist) == len(velocitylist):
+        raise ValueError("The lists don't match the provided number of arms")
+
+    iteration = 0
+    while 1:
+
+        tk.update()
+
+        iteration += 1
+
+        #Empty the list of points
+        pointlist = []
+        pointlist.append((0, 0))
+
+        #Create a list of the final rotation degrees for each point
+        rotations = []
+        for vel in velocitylist:
+            rotations.append(vel*iteration)
+
+
+        for n in range(arms):
+            point = tuple(map(sum,zip(rotate((0, lenlist[n]), rotations[n], pointlist[n]))))
+            pointlist.append(point)
+
+        for point in pointlist:
+            create_point(point)
+        for n in range(arms):
+            print pointlist[n], pointlist[n+1]
+
+
+
+
+#drawSpiral(2, [50, 50], [5, 5])
+
+x, y = 0, 0
+xstep, ystep = 5, 8.8
+lines = []
+
+while 1:
+
+    point1 = rotate((0,50), x)
+    point2 = map(sum,zip(rotate((0, 50), y), point1))
+    #create_point(point1)
+    #create_point(point2)
+    if x == 0:
+        oldpoint2 = point2
+    else:
+        canvas.create_line(oldpoint2[0], oldpoint2[1], point2[0], point2[1])
+    lines.append( canvas.create_line(0, 0, point1[0], point1[1], fill="#5CE6E6", width=3.0) )
+    lines.append( canvas.create_line(point1[0], point1[1], point2[0], point2[1], fill="#5CE6E6", width=3.0) )
+    oldpoint2 = point2
+
+    tk.update()
+
+    x += xstep
+    if x > 360 and y > 360:
+        x -= 360
+        canvas.delete("all")
+        time.sleep(1)
+    y += ystep
+    if y > 360: y -= 360
+
+    for line in lines:
+        canvas.delete(line)
+    lines = []
+    time.sleep(0.01)
+
+
+
+
+
+
+
+
+
+
+
+
 
 def drawSpiral(arms, lenlist, velocitylist):
     assert arms == len(lenlist) == len(velocitylist)
@@ -43,37 +132,4 @@ def drawSpiral(arms, lenlist, velocitylist):
         lines = []
         pointlist = []
         pointlist.append((0, 0))
-
-drawSpiral(2, [50, 75], [1, 5])
-
-
-x, y = 0, 0
-lines = []
-
-while 1:
-
-    point1 = rotate((0,25), x)
-    point2 = map(sum,zip(rotate((0, 40), -y), point1))
-    if x == 0:
-        oldpoint2 = point2
-    else:
-        canvas.create_line(oldpoint2[0], oldpoint2[1], point2[0], point2[1])
-    lines.append( canvas.create_line(0, 0, point1[0], point1[1]) )
-    lines.append( canvas.create_line(point1[0], point1[1], point2[0], point2[1]) )
-    oldpoint2 = point2
-
-    tk.update()
-
-    x += 1
-    if x > 360 and y > 360:
-        x -= 360
-        canvas.delete("all")
-        time.sleep(1)
-    y += 25
-    if y > 360: y -= 360
-
-    for line in lines:
-        canvas.delete(line)
-    lines = []
-    time.sleep(0.025)
-
+    time.sleep(0.05)
